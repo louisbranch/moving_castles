2167a288efa8ead65dee1ca22c381f6855ae3da4
Luiz Branco <me@luizbranco.com>
2013-03-31 12:59:39 -0300

    Add initial hero class

---
+++ test/hero_test.dart
@@ -0,0 +1,16 @@
+import 'package:unittest/unittest.dart';
+import '../src/hero.dart';
+
+void main() {
+
+  group('New Hero', () {
+    var hero = new Hero(name: 'Minsc');
+
+    test('has a name', () => expect('Minsc', hero.name));
+    test('level defaults to 1', () => expect(1, hero.level));
+    test('starts with 0 xp', () => expect(0, hero.xp));
+
+  });
+
+}
+
