63771ac1af4eeddfab63cc975dd82935c81b6027
Luiz Branco <me@luizbranco.com>
2013-03-31 13:29:01 -0300

    Add hire/dismiss heroes

--- src/castle.dart
+++ src/castle.dart
@@ -1,14 +1,26 @@
 library castle;
+import 'hero.dart';
 
 class Castle {
   String name;
   int x;
   int y;
+  Set<Hero> heroes;
 
-  Castle({this.name, this.x: 5, this.y: 5}) {}
+  Castle({this.name, this.x: 5, this.y: 5}) {
+    this.heroes = new Set();
+  }
 
   int dimensions() {
     return this.x * this.y;
+  }
+
+  void hire(Hero hero) {
+    this.heroes.add(hero);
+  }
+
+  void dismiss(Hero hero) {
+    this.heroes.remove(hero);
   }
 
 }
--- test/castle_test.dart
+++ test/castle_test.dart
@@ -1,5 +1,6 @@
 import 'package:unittest/unittest.dart';
 import '../src/castle.dart';
+import '../src/hero.dart';
 
 void main() {
 
@@ -10,6 +11,23 @@ void main() {
     test('x size defaults to 5', () => expect(5, castle.x));
     test('y size defaults to 5', () => expect(5, castle.y));
     test('dimensions is x * y', () => expect(25, castle.dimensions()));
+
+  });
+
+  group('Castle heroes', () {
+    var castle = new Castle();
+    var hero = new Hero();
+
+    test('hire', () {
+      castle.hire(hero);
+      expect(castle.heroes.first, hero);
+    });
+
+    test('dismiss', () {
+      castle.hire(hero);
+      castle.dismiss(hero);
+      expect(castle.heroes.isEmpty, true);
+    });
 
   });
 
