875d0bd4e46df74b1b4e9be1dffb2fe979512d7f
Luiz Branco <me@luizbranco.com>
2013-04-11 22:36:19 -0300

    Add initial layer tests

--- test/layer_test.dart
+++ test/layer_test.dart
@@ -2,17 +2,31 @@ import 'package:unittest/unittest.dart';
 import '../web/dart/layer.dart';
 import '../web/dart/tile.dart';
 
+makeTiles(n) {
+  List tiles = [];
+  while (tiles.length < n) { tiles.add(new Tile()); }
+  return tiles;
+}
+
 void main() {
 
   group('New Layer:', () {
-    var tiles = [];
-    while (tiles.length < 25) { tiles.add(new Tile()); }
-    var layer = new Layer('Floor', '/floor.png', tiles);
+    var layer = new Layer('Floor', '/floor.png', makeTiles(2), 2, 1);
 
     test('has a name', () => expect(layer.name, 'Floor'));
-    test('width size defaults to 5', () => expect(layer.width, 5));
-    test('height size defaults to 5', () => expect(layer.height, 5));
+    test('has a tile set path', () => expect(layer.tileset, '/floor.png'));
+    test('has a list of tiles', () => expect(layer.tiles.length, 2));
+    test('has width', () => expect(layer.width, 2));
+    test('has height', () => expect(layer.height, 1));
 
+  });
+
+  group('Layer tiles:', () {
+    newLayer(n) => new Layer('Floor', '/floor.png', makeTiles(n), 3, 3);
+
+    test('throws if tiles < width * height', () => expect(() => newLayer(8), throws));
+    test('throws if tiles > width * height', () => expect(() => newLayer(10), throws));
+    test('passes if tiles == width * height', () => expect(() => newLayer(9), returnsNormally));
   });
 
 }
--- web/dart/layer.dart
+++ web/dart/layer.dart
@@ -6,10 +6,12 @@ class Layer {
   final int width, height;
   final List<Tile> _tiles;
 
-  Layer(this.name, this.tileset, this._tiles, {this.width: 5, this.height: 5}) {
+  Layer(this.name, this.tileset, this._tiles, this.width, this.height) {
     if (_tiles.length != width * height) {
       throw new ExpectException("Number of tiles doesn't match area size");
     }
   }
+
+  get tiles => _tiles;
 
 }
