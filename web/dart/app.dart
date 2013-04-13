10ff56263105e1fbededa8df4e5e5937a46b9eeb
Luiz Branco <me@luizbranco.com>
2013-04-13 08:18:23 -0300

    Add working example of web component using dart sample

--- web/dart/app.dart
+++ web/dart/app.dart
@@ -1,3 +1,14 @@
 library app;
+import 'package:web_ui/web_ui.dart';
+import 'dart:html';
+import 'grid.dart';
 
-void main() {}
+void main() {
+  var grid = new Grid()
+    ..host = new DivElement()
+    ..name = 'component created in code';
+
+  var lifecycleCaller = new ComponentItem(grid)..create();
+  document.body.nodes.add(grid.host);
+  lifecycleCaller.insert();
+}
--- web/dart/grid.dart
+++ web/dart/grid.dart
@@ -2,4 +2,5 @@ library grid;
 import 'package:web_ui/web_ui.dart';
 
 class Grid extends WebComponent {
+  String name;
 }
