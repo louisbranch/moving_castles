2fe3faaf02d7ec5ea9999e33345de300d183aff4
Luiz Branco <me@luizbranco.com>
2013-04-13 11:33:54 -0300

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -r web/out
-mkdir -p web/out/dart
-cp web/dart/*.dart web/out/dart
+mkdir web/out
+cp -rf web/dart web/out/dart
 dart --package-root=packages/ packages/web_ui/dwc.dart --out web/out web/index.html
