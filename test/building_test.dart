27d2ec5af5e3222e3e652d683e7a5a80c3854f5a
Luiz Branco <me@luizbranco.com>
2013-04-17 00:53:40 -0300

    Make building width and height obrigatory

--- test/building_test.dart
+++ test/building_test.dart
@@ -4,11 +4,11 @@ import '../web/dart/building.dart';
 void main() {
 
   group('New Building:', () {
-    var building = new Building('Forge', []);
+    var building = new Building('Forge', [], 2, 2);
 
     test('has a name', () => expect('Forge', building.name));
-    test('width size defaults to 2', () => expect(2, building.width));
-    test('height size defaults to 2', () => expect(2, building.height));
+    test('has a width size', () => expect(2, building.width));
+    test('has a height size', () => expect(2, building.height));
     test('has list of layers', () => expect(building.layers.isEmpty, true));
 
   });
