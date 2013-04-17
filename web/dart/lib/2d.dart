fce61658cb99a84d27c7a7e322a1b16446bb69d5
Luiz Branco <me@luizbranco.com>
2013-04-17 20:01:20 -0300

    Rename vars in 2d lib

--- web/dart/lib/2d.dart
+++ web/dart/lib/2d.dart
@@ -1,12 +1,12 @@
 library twod;
 
 List createGrid(List layers, width, height) {
-  List temp = [];
+  List grid = [];
   int offset = 0;
   for(int i = 0; i < height; i++) {
     var row = layers.sublist(offset, offset + width);
-    temp.add(row);
+    grid.add(row);
     offset += width;
   }
-  return temp;
+  return grid;
 }
