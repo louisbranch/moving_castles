647fa834d0b0ee1735b3aef1d9bc7628eaee38c1
Luiz Branco <me@luizbranco.com>
2013-04-18 22:47:26 -0300

    Start refactoring 2d lib

--- test/lib/2d_test.dart
+++ test/lib/2d_test.dart
@@ -1,19 +1,25 @@
 import 'package:unittest/unittest.dart';
 import '../../web/dart/lib/2d.dart' as twod;
+import '../../web/dart/layer.dart';
+import '../../web/dart/tile.dart';
 
 void main() {
 
   group('grid list', () {
     int width = 3;
     int height = 2;
-    List layers = [1,2,3,4,5,6];
+    var tile = new Tile(1,1);
+    var tiles = [tile, tile, tile, tile, tile, tile];
 
-    var list = twod.createGrid(layers, width, height);
+    var layer = new Layer('bg', 'bg.png', tiles, width, height);
+    var grid = twod.createGrid([layer, layer]);
 
-    test('rows are equal to height', () => expect(list.length, height));
-    test('columns are equal to width', () => expect(list[0].length, width));
-    test('columns have elements in order', () => expect(list[1], [4,5,6]));
+    group('layer', () {
+      var layer = grid.first;
+      test('rows are equal to height', () => expect(layer.length, height));
+      test('columns are equal to width', () => expect(layer[0].length, width));
+      //test('columns have elements in order', () => expect(grid[1], [4,5,6]));
+    });
 
   });
-
 }
--- web/dart/lib/2d.dart
+++ web/dart/lib/2d.dart
@@ -1,12 +1,14 @@
 library twod;
 
-List createGrid(List layers, width, height) {
+List createGrid(List layers) => layers.map(_to2D).toList();
+
+List _to2D(layer) {
   List grid = [];
   int offset = 0;
-  for(int i = 0; i < height; i++) {
-    var row = layers.sublist(offset, offset + width);
+  for(int i = 0; i < layer.height; i++) {
+    var row = layer.tiles.sublist(offset, offset + layer.width);
     grid.add(row);
-    offset += width;
+    offset += layer.width;
   }
   return grid;
 }
