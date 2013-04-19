7b94640a8a41a68bedac4e842a307216c7fe9af9
Luiz Branco <me@luizbranco.com>
2013-04-18 23:22:48 -0300

    Add test for list of layers

--- test/lib/2d_test.dart
+++ test/lib/2d_test.dart
@@ -5,20 +5,25 @@ import '../../web/dart/tile.dart';
 
 void main() {
 
-  group('grid list', () {
+  group('grid', () {
+
     int width = 3;
     int height = 2;
-    var tile = new Tile(1,1);
-    var tiles = [tile, tile, tile, tile, tile, tile];
+    var tile1 = new Tile(1,1);
+    var tile2 = new Tile(1,1);
+    var tile3 = new Tile(1,1);
+    var tiles = [tile1, tile1, tile1, tile1, tile2, tile3];
 
     var layer = new Layer('bg', 'bg.png', tiles, width, height);
     var grid = twod.createGrid([layer, layer]);
+
+    test('has a list of layers', () => expect(grid.length, 2));
 
     group('layer', () {
       var layer = grid.first;
       test('rows are equal to height', () => expect(layer.length, height));
       test('columns are equal to width', () => expect(layer[0].length, width));
-      //test('columns have elements in order', () => expect(grid[1], [4,5,6]));
+      test('columns have elements in order', () => expect(layer[1], [tile1, tile2, tile3]));
     });
 
   });
