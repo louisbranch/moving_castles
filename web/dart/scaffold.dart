82c68c2db14f160d59a684d4cd961b5d0fdc9705
Luiz Branco <me@luizbranco.com>
2013-04-18 23:28:09 -0300

    Update grid component to match new 2d specs

--- web/dart/scaffold.dart
+++ web/dart/scaffold.dart
@@ -28,6 +28,32 @@ String castle = '''{
       ],
       "width" : 2,
       "height" : 2
+    },
+    {
+      "name" : "background",
+      "tileset" : "castle.png",
+      "tiles" : [
+        {
+          "x" : 3,
+          "y" : 2
+        },
+        {
+          "x" : 1,
+          "y" : 1,
+          "collision" : false
+        },
+        {
+          "x" : 2,
+          "y" : 1,
+          "collision" : false
+        },
+        {
+          "x" : 1,
+          "y" : 1
+        }
+      ],
+      "width" : 2,
+      "height" : 2
     }
   ],
   "width" : 2,
