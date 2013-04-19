335efa2d393bacc778c8d9a58f51d50304f6302f
Luiz Branco <me@luizbranco.com>
2013-04-18 22:48:03 -0300

    Simplify grid attrs

--- web/dart/grid.dart
+++ web/dart/grid.dart
@@ -4,12 +4,10 @@ import 'structure.dart';
 import 'lib/2d.dart' as twod;
 
 class Grid extends WebComponent {
-  List _layers = [];
-
-  get layers => _layers;
+  List layers;
 
   void from(Structure struct) {
-    _layers = twod.createGrid(struct.layers, struct.width, struct.height);
+    layers = twod.createGrid(struct.layers);
   }
 
 }
