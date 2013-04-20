9400698f263815bef47dc8d21bcb07fb29ccd657
Luiz Branco <me@luizbranco.com>
2013-04-20 20:58:39 -0300

    Remove hero class (for now)

--- test/main.dart
+++ test/main.dart
@@ -3,14 +3,12 @@ library main;
 import 'package:unittest/unittest.dart';
 import 'building_test.dart' as building;
 import 'castle_test.dart' as castle;
-import 'hero_test.dart' as hero;
 import 'tile_test.dart' as tile;
 import 'lib/serializer_test.dart' as serializer;
 
 main() {
   group("bulding", building.main);
   group("castle", castle.main);
-  group("hero", hero.main);
   group("tile", tile.main);
   group("serializer", serializer.main);
 }
