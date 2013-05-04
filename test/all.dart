b4a4ddf2ca8868dddfac693aafb6a24c1f332a4f
Luiz Branco <me@luizbranco.com>
2013-05-04 10:37:24 -0300

    Use compact test output

--- test/all.dart
+++ test/all.dart
@@ -1,10 +1,12 @@
 library test;
 import 'package:unittest/unittest.dart';
+import 'package:unittest/compact_vm_config.dart';
 import 'castle_test.dart' as castle;
 import 'tile_test.dart' as tile;
 import 'building_test.dart' as building;
 
 void main () {
+  useCompactVMConfiguration();
   group('[castle]', () => castle.main());
   group('[tile]', () => tile.main());
   group('[building]', () => building.main());
